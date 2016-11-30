b787d997a1c7be7a97ab63f2e20f7072ecd9877d
mahikero <federicoparassarmiento@yahoo.com>
2016-11-30 07:39:17 -0500

    up

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+var https = require("https");
+var username = "mahikero01";
+
+function printMessage(username, badgeCount, points) {
+    var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript";
+    console.log(message);
+};
+
+var request = https.get("https://teamtreehouse.com/" + username + ".json", function(response) {
+    console.log(response.statusCode);
+});
+
